da019146fffede579c34dc8b6f36b7b43e00f2e4
ram110406r <rrpijju11@gmail.com>
2026-04-26 00:24:09 +0530

    feat: remove CaseViewer and related case management components to streamline the application

--- src/app/c/[caseId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { CaseViewer } from "@/components/platform/CaseViewer";
-
-export default async function PublicCasePage({ params }: { params: Promise<{ caseId: string }> }) {
-  const { caseId } = await params;
-  return <CaseViewer caseId={caseId} />;
-}
