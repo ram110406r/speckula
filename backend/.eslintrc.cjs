f4b36c448adc723e5dd34577ec3b59f3a6828da1
ram110406r <rrpijju11@gmail.com>
2026-04-28 23:34:25 +0530

    dfs

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,24 +1,31 @@
 module.exports = {
-  extends: ['eslint:recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['dist/', 'node_modules/', 'prisma/migrations/'],
   env: {
     node: true,
     es2020: true,
   },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['eslint:recommended'],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
       },
-    ],
-    'no-console': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
+        'no-console': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+      },
+    },
+  ],
 };
